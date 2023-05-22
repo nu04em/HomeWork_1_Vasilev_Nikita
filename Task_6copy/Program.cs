8a4059407ba8b6c9075c01b8412fa1569d7aa7a7
Nikita <nikitarusvasilev@gmail.com>
2023-05-22 03:25:36 +0300

    commited Tasks 2, 4, 6, 8

---
+++ Task_6copy/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 0)
+    {
+        Console.WriteLine("Ноль");
+        return;
+    }
+if (num % 2 != 0)
+    Console.WriteLine("Нечетное");
+else
+    Console.Write("Четное");
